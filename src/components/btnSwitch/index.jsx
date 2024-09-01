094be1d5e302b6e599e694cf2c9910afcfd3dc36
Marina Elizarova <seagirl1110@gmail.com>
2024-09-01 20:37:37 +0300

    feat: add switched language

--- src/components/btnSwitch/index.jsx
+++ src/components/btnSwitch/index.jsx
@@ -1,5 +1,12 @@
+import { useContext } from 'react';
 import styles from './styles.module.css';
+import LanguageContext from '../../context/LanguageContext';
 
 export default function BtnSwitch() {
-  return <button></button>;
+  const { lang, setLang } = useContext(LanguageContext);
+  return (
+    <button onClick={() => setLang(lang === 'en' ? 'ru' : 'en')}>
+      Switch language
+    </button>
+  );
 }
--- src/components/textComponent/index.jsx
+++ src/components/textComponent/index.jsx
@@ -1,5 +1,14 @@
+import { useContext } from 'react';
 import styles from './styles.module.css';
+import LanguageContext from '../../context/LanguageContext';
 
 export default function TextComponent() {
-  return <p></p>;
+  const { lang } = useContext(LanguageContext);
+  return (
+    <p>
+      {lang === 'en'
+        ? 'English language was choosen'
+        : 'Был выбран русский язык'}
+    </p>
+  );
 }
